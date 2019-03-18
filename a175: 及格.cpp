082c723c60031c94b3b1b47d7a63e99b10e35138
iamagoodkid <48684434+iamagoodkid@users.noreply.github.com>
2019-03-18 22:47:56 +0800

    Create a175: 及格.cpp

---
+++ a175: 及格.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+int x ;//設定一個變數 
+cin >>  x  ;
+if(x>=60)//判斷X是否及格 
+cout <<"PASS" ;//及格輸出PASS 
+if(x<60)
+cout<<"FAIL"  ;//不及格則輸出FAIL 
+}
